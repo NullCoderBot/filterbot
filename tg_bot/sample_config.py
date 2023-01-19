258cfb9e6172755ff2488628bd21ebd031758741
NullCoderBot <120970173+NullCoderBot@users.noreply.github.com>
2023-01-19 19:49:26 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,12 +10,12 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "5894334482:AAFfm0H_vpjAn1Vg9UWb6sYiRJwxXKeqW-s"
+    OWNER_ID = "5962096905"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "DEVU_86"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://sukoon:sukoon@localhost:5432/sukoon'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
@@ -23,12 +23,12 @@ class Config(object):
     URL = None
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    SUDO_USERS = [5962096905]  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = [5962096905]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = [5962096905]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
-    PORT = 5000
+    PORT = 20000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_GBAN = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
@@ -37,7 +37,7 @@ class Config(object):
     BMERNU_SCUT_SRELFTI = 0
 
 class Production(Config):
-    LOGGER = False
+    LOGGER = True
 
 
 class Development(Config):
